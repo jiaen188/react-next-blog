12fede232c03a4c9e269d51327be14085e0cb307
gulifan <gulifan@dxy.cn>
2023-02-06 14:36:59 +0800

    feat: 设置服务端 session

--- pages/api/user/sendVerifyCode.ts
+++ pages/api/user/sendVerifyCode.ts
@@ -1,11 +1,16 @@
 import { format } from 'date-fns';
 import md5 from 'md5';
 import { encode } from 'js-base64';
+import { withIronSessionApiRoute  } from 'iron-session/next';
+import {ISession} from 'pages/api/index'
 import request from 'service/fetch';
+import { ironOptions } from 'config';
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-async function handler (req: NextApiRequest, res: NextApiResponse) {
+async function sendVerifyCode (req: NextApiRequest, res: NextApiResponse) {
+  const session: ISession = req.session;
+
   // 文档
   // http://doc.yuntongxun.com/space/5a5098313b8496dd00dcdd7f
   const { to, templateId = 1 } = req.body;
@@ -38,10 +43,23 @@ async function handler (req: NextApiRequest, res: NextApiResponse) {
 
   console.log(response);
 
-  res.status(200).json({
-    code: 0,
-    data: 123
-  }) 
+  const {statusCode, statusMsg, templateSMS} = response as any;
+  if (statusCode === '000000') {
+    session.verifyCode = verifyCode;
+    await session.save();
+    res.status(200).json({
+      code: 0,
+      msg: statusMsg,
+      data: {
+        ...templateSMS
+      }
+    }) 
+  } else {
+    res.status(200).json({
+      code: statusCode,
+      msg: statusMsg
+    }) 
+  }
 }
 
-export default handler
+export default withIronSessionApiRoute(sendVerifyCode, ironOptions)
